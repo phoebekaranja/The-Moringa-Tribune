8fe9d4a7659f087e3f7eb127ea3e24142f7968b7
phoebekaranja <phoebemurish@gmail.com>
2019-01-30 09:45:53 +0300

    second commits

---
+++ news/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome')
+    ]
+# #.........
+urlpatterns=[
+    #......
+    url('^today/$',views.news_of_day,name='newsToday')
+    ]
+urlpatterns=[
+#.........
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    ] 
