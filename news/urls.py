e126bd19789fac37d7a39e85f045f89f6c6fd894
phoebekaranja <phoebemurish@gmail.com>
2019-02-04 18:31:01 +0300

    making some changes to some files

--- news/urls.py
+++ news/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome')
-    ]
-# #.........
-urlpatterns=[
-    #......
-    url('^today/$',views.news_of_day,name='newsToday')
-    ]
+
 urlpatterns=[
 #.........
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    url('^today/$',views.news_of_day,name='newsToday'),
+
     ]
