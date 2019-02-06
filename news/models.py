b030ab8d5c548c72027896281ceda3aa164c1d43
phoebekaranja <phoebemurish@gmail.com>
2019-02-06 17:29:28 +0300

    making some changes

--- news/models.py
+++ news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
+    first_name=models.CharField(max_length =30)
+    last_name=models.CharField(max_length =30)
     email = models.EmailField()
+    def __str__(self):
+        return self.first_name
