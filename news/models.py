a09c65e45a114ed8e7cbe3e3af8834dd4ec72ab1
phoebekaranja <phoebemurish@gmail.com>
2019-02-07 11:03:56 +0300

    making some migrations to some specific files

--- news/models.py
+++ news/models.py
@@ -8,8 +8,13 @@ class Editor(models.Model):
         return self.first_name
     class Meta:
         ordering = ['first_name']
-try:
-    editor = Editor.objects.get(email = 'example@gmail.com')
-    print('Editor found')
-except DoesNotExist:
-    print('Editor was not found')
+# try:
+#     editor = Editor.objects.get(email = 'example@gmail.com')
+#     print('Editor found')
+# except DoesNotExist:
+#     print('Editor was not found')
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
