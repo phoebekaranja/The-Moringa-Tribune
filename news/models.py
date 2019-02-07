bcc6f49ed43affe5ad51d726bdb5135d1927d465
phoebekaranja <phoebemurish@gmail.com>
2019-02-07 11:27:33 +0300

    modifying models and test files

--- news/models.py
+++ news/models.py
@@ -18,3 +18,9 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
