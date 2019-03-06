7cf3b8259ee22568a4347fd0c22a768fd38b8fcf
phoebekaranja <phoebemurish@gmail.com>
2019-03-06 16:16:22 +0300

    modifying some files

--- news/views.py
+++ news/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .models import Article
 from .forms import NewsLetterForm
 import datetime as dt
-
+from .email import send_welcome_email
 # Create your views here.
 
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html',{"date": date,"news":news
-    })
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html',{"date": date,"news":news,"letterForm":form})
 
 def search_results(request):
 
