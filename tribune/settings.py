887212d595ff0c7a68d56fc8558524559cefe5d4
phoebekaranja <phoebemurish@gmail.com>
2019-03-06 16:05:11 +0300

    modifying some files

--- tribune/settings.py
+++ tribune/settings.py
@@ -118,6 +118,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 LANGUAGE_CODE = 'en-us'
 
